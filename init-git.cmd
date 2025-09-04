88943c4a4bbd6af555140d5ed67b23a0d4aa5a56
igen <nem>
2025-09-04 07:19:53 +0200

    2025. 09. 04. - Els┼Ĺ commit

---
+++ init-git.cmd
@@ -0,0 +1,7 @@
+echo "restapi-orak" >> README.md
+git init
+git add .
+git commit -m "%date% - Első commit"
+git branch -M main
+git remote add origin https://github.com/HelloandMore/restapi-orak.git
+git push -u origin main
