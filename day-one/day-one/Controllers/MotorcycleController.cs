cbdb9d83afc2e1000df788cdf2b6d3316a363c0c
igen <nem>
2025-09-04 09:31:55 +0200

    2025. 09. 04. - háziként egy hasonlót kéne készíteni

---
+++ day-one/day-one/Controllers/MotorcycleController.cs
@@ -0,0 +1,60 @@
+﻿using day_one.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace day_one.Controllers;
+
+public class MotorcycleController: ControllerBase
+{
+    public List<string> Motorcycles = [
+        "Honda Hornet",
+        "Triumph Tiger valami",
+        "Sport, Az",
+        "Valami Yamaha"
+        ];
+
+    [HttpGet]
+    [Route("motorcycle/id/{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] [Required] int id)
+    {
+        return Ok($"hello! {Motorcycles[id]}");
+    }
+
+    [HttpGet]
+    [Route("motorcycle")]
+    public async Task<IActionResult> GetAsync([FromQuery] [Required] int id)
+    {
+        return Ok($"hello! {Motorcycles[id]}");
+    }
+
+    [HttpGet]
+    [Route("motorcycle/all")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        return Ok(Motorcycles);
+    }
+
+    [HttpPost]
+    [Route("motorcycle/create")]
+    public async Task<IActionResult> CreateAsync([FromBody] [Required] string motorcycle)
+    {
+        Motorcycles.Add(motorcycle);
+        return Ok($"Successfully added {motorcycle} to database!");
+    }
+
+    [HttpPut]
+    [Route("motorcycle/update")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] [Required] MotorcycleUpdateModel model)
+    {
+        Motorcycles[model.Id] = model.Name;
+        return Ok($"Successfully updated motorcycle with id {model.Id} to {model.Name}!");
+    }
+
+    [HttpDelete]
+    [Route("motorcycle/delete/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] [Required] int id)
+    {
+        Motorcycles.RemoveAt(id);
+        return Ok($"Success: {Motorcycles.ToString()}");
+    }
+}
