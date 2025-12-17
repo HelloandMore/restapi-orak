8fe4650b7c734d4a277b4ab4698f292217ae2c25
igen <nem>
2025-12-17 08:45:59 +0100

    Add project references and clean up unused files in Solution.Core

--- 04 - Bills/Solution.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Solution.Core
-{
-	public class Class1
-	{
-
-	}
-}
---
+++ 04 - Bills/Solution.Core/GlobalImports.cs
@@ -0,0 +1,8 @@
+﻿global using ErrorOr;
+global using Solution.Database;
+global using Solution.Database.Entities;
+global using Solution.Core.Models;
+global using Solution.Core.Interfaces;
+global using System.Text.Json.Serialization;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using Microsoft.EntityFrameworkCore;
