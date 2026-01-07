998fac9408aced7d01953b669258a81638dc7f76
igen <nem>
2026-01-07 09:21:31 +0100

    2026. 01. 07. - even more painful stuff

---
+++ 02 - Auth/Solution.Domain/Database/Builders/UserEntityModeBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace Solution.Domain.Database.Builders;
+
+internal static class UserEntityModeBuilder
+{
+    public static void ConfigureUser(this ModelBuilder builder)
+    {
+        builder.Entity<UserEntity>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasIndex(x => x.Id)
+                  .IsUnique();
+
+            entity.Property(x => x.FullName)
+                  .HasColumnName("FullName")
+                  .HasMaxLength(255)
+                  .IsRequired();
+            entity.Property(x => x.RegisteredUtc)
+                  .HasColumnName("RegisteredUtc")
+                  .IsRequired();
+        }
+        );
+    }
+}
---
+++ 02 - Auth/Solution.Domain/GlobalImports.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Text;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
+global using Solution.Domain.Database.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Solution.Domain.Database.Builders;
+global using System.Reflection;
