fb041dca26f8d7606ee2c3a1262d5bef8e579995
Alt+3♥ <nem>
2026-03-26 07:20:42 +0100

    2026. 03. 26. - tasks pushed

---
+++ dolgozatok/Kondor Milan/StarWarsApi/StarWarsApi.Database/Migrations/20260325081649_init.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarWarsApi.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderType = table.Column<int>(type: "int", nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Homeworld = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Era = table.Column<int>(type: "int", nullable: false),
+                    Rank = table.Column<int>(type: "int", nullable: false),
+                    LightsaberColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Master = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apprentice = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ForceSpecialty = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAlive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Character");
+        }
+    }
+}
